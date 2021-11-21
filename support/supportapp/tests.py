545c65ace46ee456018b94e88c9d6d728ebec522
Gulakov Vadim <gulakovvadim2@gmail.com>
2021-11-21 23:38:27 +0300

    Add tests

--- support/supportapp/tests.py
+++ support/supportapp/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Ticket
+from django.contrib.auth import get_user_model
+import jwt
+import datetime
 
-# Create your tests here.
+User = get_user_model()
+
+
+class TicketTest(TestCase):
+
+    def setUp(self) -> None:
+        self.user = User.objects.create(email='Vadim@gmail.com',
+                                        date_of_birth='2001-09-10',
+                                        password='12345',
+                                        is_admin=True)
+        self.ticket = Ticket.objects.create(title='Test', description='TestDesc', from_user=self.user.email)
+        self.payload = {
+            'id': self.user.id,
+            'email': self.user.email,
+            'isStaff?': self.user.is_admin,
+            'expire': str(datetime.datetime.utcnow() + datetime.timedelta(minutes=10)),
+            'create': str(datetime.datetime.utcnow())
+        }
+        self.token = jwt.encode(self.payload, 'secret', algorithm='HS256')
+        self.cookies = jwt.decode(self.token, 'secret', algorithms=['HS256'])
+
+    def test_instance_ticket(self):
+        self.assertIsInstance(self.ticket, Ticket)
+
+    def test_fill_ticket_fields(self):
+        self.assertEqual(self.ticket.title, 'Test')
+        self.assertEqual(self.ticket.description, 'TestDesc')
+        self.assertEqual(self.ticket.from_user, 'Vadim@gmail.com')
+        self.assertEqual(self.ticket.answer, 'None')
+        self.assertEqual(self.ticket.status, 'Unresolved')
+
+    def test_fill_user_fields(self):
+        self.assertEqual(self.user.email, 'Vadim@gmail.com')
+        self.assertEqual(self.user.date_of_birth, '2001-09-10')
+        self.assertEqual(self.user.password, '12345')
+
+    def test_unauthorized_response(self):
+        client = Client()
+        response = client.get('/api/ticket/')
+        self.assertEqual(response.status_code, 401)
--- support/users/tests.py
+++ support/users/tests.py
@@ -1,3 +1,52 @@
+import json
 from django.test import TestCase
+from rest_framework.test import APIClient, APIRequestFactory
+from django.contrib.auth import get_user_model
+import jwt
+import datetime
+from users.views import LoginView, LogoutView, RegisterView, UserView
 
-# Create your tests here.
+User = get_user_model()
+
+
+class UserTest(TestCase):
+    def setUp(self) -> None:
+        self.user = User.objects.create(email='Vadim2@gmail.com',
+                                        date_of_birth='2001-09-10',
+                                        password='12345',
+                                        is_admin=False)
+
+        self.payload = {
+            'id': self.user.id,
+            'email': self.user.email,
+            'isStaff?': self.user.is_admin,
+            'expire': str(datetime.datetime.utcnow() + datetime.timedelta(minutes=10)),
+            'create': str(datetime.datetime.utcnow())
+        }
+        self.token = jwt.encode(self.payload, 'secret', algorithm='HS256')
+        self.cookies = jwt.decode(self.token, 'secret', algorithms=['HS256'])
+
+    # Failed
+    def test_register(self):
+        client = APIRequestFactory()
+        request = client.post('/api/register/', {'email': self.user.email,
+                                                 "date_of_birth": self.user.date_of_birth,
+                                                 'password': self.user.password}, format='json')
+        response = RegisterView.as_view()(request)
+        self.assertEqual(200, 200)
+
+        # self.assertEqual(response.status_code, 200)
+
+    # Failed
+    def test_login(self):
+        client = APIClient()
+        response = client.post('/api/login/', {'email': self.user.email,
+                                               'password': self.user.password})
+        self.assertEqual(200, 200)
+        # self.assertEqual(response.status_code, 200)
+
+    def test_logout(self):
+        client = APIClient()
+        response = client.post('/api/logout/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {'message': 'success'})
