20e857c537f86f66cd35546ab9cc258582775ccb
Gulakov Vadim <gulakovvadim2@gmail.com>
2021-11-22 15:40:21 +0300

    Update ticket model

--- support/supportapp/models.py
+++ support/supportapp/models.py
@@ -12,7 +12,8 @@ class Ticket(models.Model):
     description = models.CharField(max_length=300, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     from_user = models.CharField(max_length=255, default="Unknown")
-    answer = models.CharField(max_length=255, default='None')
+    support_answer = models.CharField(max_length=255, default='None')
+    user_answer = models.CharField(max_length=255, default='None')
     status = models.CharField(max_length=10, choices=CHOICES, default='Unresolved')
 
     def __str__(self):
