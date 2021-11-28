6bdab4b80f20efc1ad848419e61a36eca4886eb4
Vadimka <79689527+vdmglkv@users.noreply.github.com>
2021-11-28 16:36:37 +0300

    Update auth.py

--- support/users/auth.py
+++ support/users/auth.py
@@ -9,7 +9,7 @@ def generate_access_token(user):
             'id': user.id,
             'email': user.email,
             'isStaff?': user.is_admin,
-            'expire': str(datetime.datetime.utcnow() + datetime.timedelta(minutes=1)),
+            'expire': str(datetime.datetime.utcnow() + datetime.timedelta(minutes=10)),
             'create': str(datetime.datetime.utcnow())
         }
     access_token = jwt.encode(access_token_payload,
